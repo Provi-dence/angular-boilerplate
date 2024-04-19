56bfe9b557e2e8316837beffe10e2d56233679d9
unknown <YY101@ccs.local>
2024-04-18 21:19:27 -0700

    '_helpers/fake-backend'

---
+++ src/app/_helpers/app.initializer.ts
@@ -0,0 +1,10 @@
+import { AccountSevice } from '@app/_services';
+
+export function appInitializer(accountService: AccountSevice) {
+    return () => new Promise(resolve => {
+        // attempt to refresh token on app start up to auto authenticate
+        accountService.refreshToken()
+            .subscribe()
+            .add(resolve);
+    });
+}
---
+++ src/app/_helpers/fake-backend.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS, HttpHeaders } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { delay, materialize, dematerialize } from 'rxjs/operators';
+
+import { AlertService } from '@app/_services';
+import { Role } from '@app/_models';
+
+// array in local storage for accounts
+const accountsKey = 'angular-10-signup-verification-boilerplate-accounts';
+let accounts = JSON.parse(localStorage.getItem(accountsKey) || '[]');
+
+@Injectable()
+export class FakeBackendInterceptor implements HttpInterceptor {
+    constructor(private alertService: AlertService) { }
+
+    HttpInterceptor(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const { url, method, headers, body } = request;
+        const alertService = this.alertService;
+
+        return handleRoute();
+
+        function handleRoute() {
+            switch(true) {
+                case url.endsWith('/accounts/authenticate') && method === 'POST':
+                    return authenticate();
+                case url.endsWith('/accounts/refresh-token') && method === 'POST':
+                    return refreshToken();
+                case url.endsWith('/accounts/revoke-token') && method === 'POST':
+                    return revokeToken();
+                case url.endsWith('/accounts/register') && method === 'POST':
+                    return register();
+            }
+        }
+}
